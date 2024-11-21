7fc729e3338a468846814aa03364311ab52765d1
Fernando3281 <fervargas2003@hotmail.com>
2024-11-21 13:14:31 -0600

    Pedidos FrontEnd

---
+++ GoshStore202403/FrontEnd/Helpers/Interfaces/IPedidoHelper.cs
@@ -0,0 +1,13 @@
+﻿using FrontEnd.Models;
+
+namespace FrontEnd.Helpers.Interfaces
+{
+    public interface IPedidoHelper
+    {
+        List<PedidoViewModel> GetPedidos();
+        PedidoViewModel GetPedido(int id);
+        PedidoViewModel Add(PedidoViewModel pedido);
+        PedidoViewModel Update(int id, PedidoViewModel pedido);
+        void Delete(int id);
+    }
+}
