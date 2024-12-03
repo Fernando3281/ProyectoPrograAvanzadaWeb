ff3b2c9872eaf9aeafd983640fd3fd5e538d4859
Aviles94 <caavilese0594@gmail.com>
2024-12-03 14:30:28 -0600

    Commit direcciones

---
+++ GoshStore202403/DAL/Implementations/DireccioneDALImpl.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class DireccioneDALImpl: DALGenericoImpl<Direccione>, IDireccioneDAL
+    {
+        DbGoshStoreContext context;
+
+        public DireccioneDALImpl(DbGoshStoreContext dbGoshStoreContext):base (dbGoshStoreContext) 
+        {
+            this.context = dbGoshStoreContext;
+        }
+    }
+}
